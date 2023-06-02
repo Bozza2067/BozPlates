f41330510b563948fd77da45dbaa7f68ddd37749
Biometrical <jacob@stevesworld.org>
2023-06-02 10:29:20 -0400

    Working config

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,5 +5,13 @@ description 'BozPlates'
 author 'Boz'
 version '1.0.0'
 
-client_script 'client.lua'
-client_script 'config.lua'
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+files {
+    'config.lua'
+}
+
+shared_script 'config.lua' -- our config
